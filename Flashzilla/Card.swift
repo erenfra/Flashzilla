3f9aaf1eee70d2bd67af27f55c12a251411475c0
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-11-12 22:36:18 -0600

    day 88 commit

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Renato Oliveira Fraga on 11/12/21.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    var answer: String
+    
+    static var example: Card {
+        Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
