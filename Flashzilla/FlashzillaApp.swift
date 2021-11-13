0c292788b8fe3729cf01809a5d9fe2d2e4bbcf64
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-11-12 21:07:26 -0600

    first commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Renato Oliveira Fraga on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
